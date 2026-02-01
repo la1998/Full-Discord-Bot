33807322c71c978e6fd0a942e33ffac53da707d4
Arty <34949778+la1998@users.noreply.github.com>
2026-02-01 03:10:36 +0100

    Delete start-all.ps1

--- start-all.ps1
+++
@@ -1,2 +0,0 @@
-Start-Process powershell -ArgumentList 'cd ./backend; npm install; node index.js' -NoNewWindow
-Start-Process powershell -ArgumentList 'cd ./frontend; npm install; npm run dev' -NoNewWindow
